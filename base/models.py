faa6b7e9e5056a94df9978b7d5e37acae271de78
zoe <zoejniu@gmail.com>
2022-01-24 16:56:23 +1300

    Create spaces model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Space(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # members =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
