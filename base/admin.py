33adbcc0c23853f85f9828e1069afabb5d72fd29
zoe <zoejniu@gmail.com>
2022-01-25 18:30:20 +1300

    Create models and relationships

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Space, Topic, Message
 
 # Register your models here.
+admin.site.register(Space)
+admin.site.register(Topic)
+admin.site.register(Message)
--- base/views.py
+++ base/views.py
@@ -1,22 +1,15 @@
+from inspect import Attribute
 from django.shortcuts import render
+from .models import Space
 
 # Create your views here.
-spaces = [
-    {"id": 1, "name": "Harry Potter"},
-    {"id": 2, "name": "FOG"},
-    {"id": 3, "name": "Bible"},
-]
-
-
 def home(request):
+    spaces = Space.objects.all()
     context = {"spaces": spaces}
     return render(request, "base/home.html", context)
 
 
 def space(request, pk):
-    space = None
-    for i in spaces:
-        if i["id"] == int(pk):
-            space = i
+    space = Space.objects.get(id=pk)
     context = {"space": space}
     return render(request, "base/space.html", context)
